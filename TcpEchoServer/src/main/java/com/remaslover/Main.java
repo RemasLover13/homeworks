1fc3efe672f01a41c2402b91fb53207cde89bf6b
Edik Krmakov <krmakov2001@mail.ru>
2025-04-04 00:34:01 +0300

    Init project

---
+++ TcpEchoServer/src/main/java/com/remaslover/Main.java
@@ -0,0 +1,10 @@
+package com.remaslover;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+    }
+}
